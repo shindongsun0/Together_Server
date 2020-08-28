0885233e98fb672f95a125d707894ac3a36ae308
Sindongsun <shindongsun0@naver.com>
2020-08-29 03:24:44 +0900

    fix: group builder 전체 class범위로 수정
    + service단 주석 수정

--- src/main/java/com/together/smwu/web/dto/group/enrollment/GroupEnrollmentDeleteResponseDto.java
+++ src/main/java/com/together/smwu/web/dto/group/enrollment/GroupEnrollmentDeleteResponseDto.java
@@ -4,20 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class GroupEnrollmentDeleteResponseDto {
 
     private Long groupId;
     private Long userId;
-
-    @Builder
-    public GroupEnrollmentDeleteResponseDto(Long groupId){
-        this.groupId = groupId;
-    }
-
-    @Builder
-    public GroupEnrollmentDeleteResponseDto(Long groupId, Long userId){
-        this.groupId = groupId;
-        this.userId = userId;
-    }
-
 }
