1d00fe1403572918efe9984cf8bec9a291b68c42
Sindongsun <shindongsun0@naver.com>
2020-09-03 02:37:02 +0900

    fix: ResponseEntity적용

--- src/main/java/com/together/smwu/domain/groupEnrollment/api/GroupEnrollmentController.java
+++ src/main/java/com/together/smwu/domain/groupEnrollment/api/GroupEnrollmentController.java
@@ -1,18 +1,19 @@
 package com.together.smwu.domain.groupEnrollment.api;
 
-import com.together.smwu.config.security.CurrentUser;
-import com.together.smwu.domain.user.domain.User;
-import com.together.smwu.domain.groupEnrollment.dto.GroupEnrollmentDeleteResponseDto;
+import com.together.smwu.domain.groupEnrollment.application.GroupEnrollmentService;
 import com.together.smwu.domain.groupEnrollment.dto.GroupEnrollmentRequestDto;
 import com.together.smwu.domain.groupEnrollment.dto.GroupEnrollmentResponseDto;
-import com.together.smwu.domain.groupEnrollment.application.GroupEnrollmentService;
+import com.together.smwu.domain.security.security.CurrentUser;
+import com.together.smwu.domain.user.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @PreAuthorize("hasRole('ROLE_USER')")
@@ -20,71 +21,66 @@ import java.util.List;
 @RestController
 @Slf4j
 public class GroupEnrollmentController {
+    public static final String GROUP_ENROLL_URI = "/api.group/enroll";
 
-    private GroupEnrollmentService groupEnrollmentService;
+    private final GroupEnrollmentService groupEnrollmentService;
 
     public GroupEnrollmentController(GroupEnrollmentService groupEnrollmentService) {
         this.groupEnrollmentService = groupEnrollmentService;
     }
 
     @ApiOperation(value = "Group에 User 등록", notes = "Group에 User를 등록한다")
-    @PostMapping(value = "/api/group/enroll")
-    public boolean enroll(
+    @PostMapping(value = GROUP_ENROLL_URI)
+    public ResponseEntity<Void> enroll(
             @RequestBody GroupEnrollmentRequestDto requestDto,
             @CurrentUser User user) {
-        return groupEnrollmentService.enroll(requestDto);
+        Long groupEnrollmentId = groupEnrollmentService.enroll(requestDto, user);
+        return ResponseEntity
+                .created(URI.create(GROUP_ENROLL_URI + "/" + groupEnrollmentId))
+                .build();
     }
 
     @ApiOperation(value = "Group의 모든 user 조회", notes = "groupId로 모든 User를 조회한다.")
     @GetMapping(value = "/api/group/enroll/all/{groupId}")
-    public List<GroupEnrollmentResponseDto> findAllUsers(
+    public ResponseEntity<List<GroupEnrollmentResponseDto>> findAllUsers(
             @ApiParam(value = "groupId", required = true) @PathVariable long groupId,
             @CurrentUser User user) {
-
-        return groupEnrollmentService.findAllByGroup(groupId);
+        List<GroupEnrollmentResponseDto> responses = groupEnrollmentService.findAllByGroup(groupId);
+        return ResponseEntity.ok(responses);
     }
 
     @ApiOperation(value = "User가 속한 모든 group 조회", notes = "userId로 등록한 모든 group을 조회한다.")
     @GetMapping(value = "/api/group/enroll/all/{userId}")
-    public List<GroupEnrollmentResponseDto> findAllGroups(
+    public ResponseEntity<List<GroupEnrollmentResponseDto>> findAllGroups(
             @ApiParam(value = "userId", required = true) @PathVariable long userId,
             @CurrentUser User user) {
-
-        return groupEnrollmentService.findAllByUser(userId);
+        List<GroupEnrollmentResponseDto> responses = groupEnrollmentService.findAllByUser(userId);
+        return ResponseEntity.ok(responses);
     }
 
-    @GetMapping("/api/group/enroll/{id}")
-    public GroupEnrollmentResponseDto findById(
-            @ApiParam(value = "groupEnrollmentId", required = true) @PathVariable Long id) {
-
-        return groupEnrollmentService.findById(id);
+    @GetMapping("/api/group/enroll/{groupEnrollmentId}")
+    public ResponseEntity<GroupEnrollmentResponseDto> findByGroupEnrollmentId(
+            @ApiParam(value = "groupEnrollmentId", required = true) @PathVariable Long groupEnrollmentId) {
+        GroupEnrollmentResponseDto groupEnrollmentResponse = groupEnrollmentService.findById(groupEnrollmentId);
+        return ResponseEntity.ok(groupEnrollmentResponse);
     }
 
     @ApiOperation(value = "Group의 모든 User 삭제", notes = "Master User라면 groupId로 모든 User를 삭제한다.")
     @DeleteMapping(value = "/api/group/enroll/delete/{groupId}")
-    public GroupEnrollmentDeleteResponseDto deleteAllUsers(
+    public ResponseEntity<Void> deleteAllUsers(
             @ApiParam(value = "groupId", required = true) @PathVariable long groupId,
             @CurrentUser User user) {
-
         groupEnrollmentService.deleteAllUsers(groupId, user);
-
-        return GroupEnrollmentDeleteResponseDto.builder()
-                .groupId(groupId)
-                .build();
+        return ResponseEntity.noContent().build();
     }
 
     @ApiOperation(value = "Group의 User 삭제", notes = "groupId와 User로 그룹을 탈퇴한다.")
     @DeleteMapping(value = "/api/group/enroll/{groupId}/delete")
-    public GroupEnrollmentDeleteResponseDto deleteUserFromGroup(
+    public ResponseEntity<Void> deleteUserFromGroup(
             @ApiParam(value = "userId", required = true) @PathVariable long groupId,
             @CurrentUser User user) {
-
         groupEnrollmentService.deleteUserFromGroup(groupId, user);
-
-        return GroupEnrollmentDeleteResponseDto.builder()
-                .userId(user.getMsrl())
-                .groupId(groupId)
-                .build();
+        return ResponseEntity.noContent().build();
     }
 }
 
