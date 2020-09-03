f1669954a7be59109499b7f2a2ed62ffcd74ac68
Sindongsun <shindongsun0@naver.com>
2020-09-03 16:39:59 +0900

    add: group Repository, not yet apply querydsl

--- src/main/java/com/together/smwu/domain/group/dao/GroupRepository.java
+++ src/main/java/com/together/smwu/domain/group/dao/GroupRepository.java
@@ -3,6 +3,7 @@ package com.together.smwu.domain.group.dao;
 import com.together.smwu.domain.group.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long>, GroupRepositoryCustom {
@@ -11,5 +12,5 @@ public interface GroupRepository extends JpaRepository<Group, Long>, GroupReposi
 
     Optional<Group> findById(Long id);
 
-    Optional<Group> findByTitle(String title);
+    List<Group> findByTitle(String title);
 }
