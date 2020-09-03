764c6b4a6bf92f65929d22c5f67cd9d1381402ec
Sindongsun <shindongsun0@naver.com>
2020-09-03 15:04:58 +0900

    add: Business Logic Exceptions add

--- src/main/java/com/together/smwu/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/together/smwu/domain/user/exception/UserNotFoundException.java
@@ -1,8 +1,8 @@
 package com.together.smwu.domain.user.exception;
 
-import com.together.smwu.global.exception.ExpectedException;
+import com.together.smwu.global.advice.exception.EntityNotFoundException;
 
-public class UserNotFoundException extends ExpectedException {
+public class UserNotFoundException extends EntityNotFoundException {
 
     private static final String MESSAGE = "존재하지 않는 사용자입니다.";
 
