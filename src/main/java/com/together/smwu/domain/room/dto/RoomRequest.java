7f6d4df816aa90997d2b68328fd255ca2f2ca403
Sindongsun <shindongsun0@naver.com>
2020-09-05 03:22:37 +0900

    add: room request, response dto

--- src/main/java/com/together/smwu/domain/room/dto/RoomRequest.java
+++ src/main/java/com/together/smwu/domain/room/dto/RoomRequest.java
@@ -1,13 +1,13 @@
-package com.together.smwu.domain.group.dto;
+package com.together.smwu.domain.room.dto;
 
-import com.together.smwu.domain.group.domain.Group;
+import com.together.smwu.domain.room.domain.Room;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
-public class GroupRequest {
+public class RoomRequest {
     private final String BASE_IMAGE_URL = "https://together-user-thumbnail.s3.ap-northeast-2.amazonaws.com/static/up.png";
 
     private String title;
@@ -16,19 +16,19 @@ public class GroupRequest {
     private String credential;
 
 
-    private GroupRequest(String title, String content, String imageUrl,
-                         String credential) {
+    private RoomRequest(String title, String content, String imageUrl,
+                        String credential) {
         this.title = title;
         this.content = content;
         this.imageUrl = imageUrl;
         this.credential = credential;
     }
 
-    public Group toGroupEntity() {
+    public Room toRoomEntity() {
         if (imageUrl.isEmpty()) {
             imageUrl = BASE_IMAGE_URL;
         }
-        return Group.builder()
+        return Room.builder()
                 .title(title)
                 .content(content)
                 .imageUrl(imageUrl)
