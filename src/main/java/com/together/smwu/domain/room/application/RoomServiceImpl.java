35236dd4bdaa67c1d5de70645b5d6a93afed5f9f
Sindongsun <shindongsun0@naver.com>
2020-09-05 22:21:12 +0900

    fix: enrollment delete위치 fix

--- src/main/java/com/together/smwu/domain/room/application/RoomServiceImpl.java
+++ src/main/java/com/together/smwu/domain/room/application/RoomServiceImpl.java
@@ -27,7 +27,7 @@ public class RoomServiceImpl implements RoomService {
 
     public Long create(RoomRequest request, User user) {
         Room room = roomRepository.save(request.toRoomEntity());
-//        enrollRoomWithMasterUser(user, room);
+        enrollRoomWithMasterUser(user, room);
         return room.getId();
     }
 
@@ -65,9 +65,9 @@ public class RoomServiceImpl implements RoomService {
 
     @Transactional
     public void deleteRoomById(Long roomId, User user) {
+        roomEnrollmentRepository.deleteAllByRoomId(roomId);
         Room authorizedRoom = findRoomIfAuthorized(roomId, user.getUserId());
         roomRepository.delete(authorizedRoom);
-        roomEnrollmentRepository.deleteAllByRoomId(roomId);
     }
 
     private void enrollRoomWithMasterUser(User user, Room room) {
