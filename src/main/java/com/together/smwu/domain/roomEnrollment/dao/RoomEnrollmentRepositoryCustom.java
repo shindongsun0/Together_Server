e282bd72de91e77d589afae576be6d1f95e8b985
Sindongsun <shindongsun0@naver.com>
2020-09-11 03:49:51 +0900

    fix: [RoomEnrollment] repository logic fix
    join 사용

--- src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryCustom.java
+++ src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryCustom.java
@@ -3,11 +3,15 @@ package com.together.smwu.domain.roomEnrollment.dao;
 import com.together.smwu.domain.room.domain.Room;
 import com.together.smwu.domain.roomEnrollment.domain.RoomEnrollment;
 
+import java.util.Optional;
+
 public interface RoomEnrollmentRepositoryCustom {
 
     long deleteAllByRoomId(long roomId);
 
     long deleteAllByRoom(Room room);
+
+    Optional<RoomEnrollment> findByUserAndRoom(long userId, long roomId);
 
     long deleteAllByUserId(long userId);
 
--- src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryImpl.java
+++ src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.together.smwu.domain.roomEnrollment.dao;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.together.smwu.domain.room.domain.QRoom;
+import com.together.smwu.domain.room.domain.Room;
+import com.together.smwu.domain.roomEnrollment.domain.QRoomEnrollment;
 import com.together.smwu.domain.roomEnrollment.domain.RoomEnrollment;
+import com.together.smwu.domain.user.domain.QUser;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.together.smwu.domain.roomEnrollment.domain.QRoomEnrollment.roomEnrollment;
 
@@ -12,6 +17,9 @@ import static com.together.smwu.domain.roomEnrollment.domain.QRoomEnrollment.roo
 public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+    private final QRoomEnrollment re = roomEnrollment;
+    private final QRoom r = QRoom.room;
+    private final QUser u = QUser.user;
 
     @Override
     public long deleteAllByRoomId(long roomId) {
@@ -19,6 +27,20 @@ public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCus
                 .delete(roomEnrollment)
                 .where(roomEnrollment.room.id.eq(roomId))
                 .execute();
+    }
+
+    @Override
+    public long deleteAllByRoom(Room room) {
+        return queryFactory.delete(re)
+                .where(re.room.eq(room))
+                .execute();
+    }
+
+    @Override
+    public Optional<RoomEnrollment> findByUserAndRoom(long userId, long roomId) {
+        return Optional.ofNullable(queryFactory.selectFrom(re)
+                .where(re.room.id.eq(roomId).and(re.user.userId.eq(userId)))
+                .fetchOne());
     }
 
     @Override
@@ -39,15 +61,13 @@ public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCus
     }
 
     @Override
-    public boolean checkIfMasterOfRoom(long roomId, long userId) {
-        List<RoomEnrollment> roomEnrollments = queryFactory
-                .select(roomEnrollment)
-                .from(roomEnrollment)
-                .where(roomEnrollment.room.id.eq(roomId)
-                        .and(roomEnrollment.user.userId.eq(userId))
-                        .and(roomEnrollment.isMaster.isTrue()))
+    public boolean checkIsMasterOfRoom(long userId, long roomId) {
+        List<RoomEnrollment> roomEnrollments = queryFactory.selectFrom(re)
+                .join(re.room, r).fetchJoin()
+                .join(re.user, u).fetchJoin()
+                .where(re.user.userId.eq(userId).and(re.room.id.eq(roomId)))
                 .fetch();
 
-        return roomEnrollments.isEmpty();
+        return !roomEnrollments.isEmpty();
     }
 }
