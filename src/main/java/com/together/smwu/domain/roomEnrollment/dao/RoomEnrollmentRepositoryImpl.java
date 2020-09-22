ca63fff7a9c93c16468fdb848fd38cd52c4165ec
Sindongsun <shindongsun0@naver.com>
2020-09-22 19:43:08 +0900

    add: [RoomEnrollment] roomDetailInfo query 작성

--- src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryImpl.java
+++ src/main/java/com/together/smwu/domain/roomEnrollment/dao/RoomEnrollmentRepositoryImpl.java
@@ -1,25 +1,28 @@
 package com.together.smwu.domain.roomEnrollment.dao;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.together.smwu.domain.room.domain.QRoom;
 import com.together.smwu.domain.room.domain.Room;
+import com.together.smwu.domain.room.dto.Master;
+import com.together.smwu.domain.room.dto.RoomDetailInfo;
 import com.together.smwu.domain.roomEnrollment.domain.QRoomEnrollment;
 import com.together.smwu.domain.roomEnrollment.domain.RoomEnrollment;
 import com.together.smwu.domain.user.domain.QUser;
+import com.together.smwu.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Optional;
-
-import static com.together.smwu.domain.roomEnrollment.domain.QRoomEnrollment.roomEnrollment;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-    private final QRoomEnrollment re = roomEnrollment;
-    private final QRoom r = QRoom.room;
-    private final QUser u = QUser.user;
+    private final QRoomEnrollment roomEnrollment = QRoomEnrollment.roomEnrollment;
+    private final QRoom room = QRoom.room;
+    private final QUser user = QUser.user;
 
     @Override
     public long deleteAllByRoomId(long roomId) {
@@ -31,15 +34,16 @@ public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCus
 
     @Override
     public long deleteAllByRoom(Room room) {
-        return queryFactory.delete(re)
-                .where(re.room.eq(room))
+        return queryFactory.delete(roomEnrollment)
+                .where(roomEnrollment.room.eq(room))
                 .execute();
     }
 
     @Override
     public Optional<RoomEnrollment> findByUserAndRoom(long userId, long roomId) {
-        return Optional.ofNullable(queryFactory.selectFrom(re)
-                .where(re.room.id.eq(roomId).and(re.user.userId.eq(userId)))
+        return Optional.ofNullable(queryFactory.selectFrom(roomEnrollment)
+                .where(roomEnrollment.room.id.eq(roomId)
+                        .and(roomEnrollment.user.userId.eq(userId)))
                 .fetchOne());
     }
 
@@ -62,12 +66,36 @@ public class RoomEnrollmentRepositoryImpl implements RoomEnrollmentRepositoryCus
 
     @Override
     public boolean checkIsMasterOfRoom(long userId, long roomId) {
-        List<RoomEnrollment> roomEnrollments = queryFactory.selectFrom(re)
-                .join(re.room, r).fetchJoin()
-                .join(re.user, u).fetchJoin()
-                .where(re.user.userId.eq(userId).and(re.room.id.eq(roomId)))
+        List<RoomEnrollment> roomEnrollments = queryFactory.selectFrom(roomEnrollment)
+                .join(roomEnrollment.room, room).fetchJoin()
+                .join(roomEnrollment.user, user).fetchJoin()
+                .where(roomEnrollment.user.userId.eq(userId)
+                        .and(roomEnrollment.room.id.eq(roomId)))
                 .fetch();
 
         return !roomEnrollments.isEmpty();
+    }
+
+    public RoomDetailInfo getMasterUser(Room targetRoom) {
+        Master master = queryFactory
+                .select(Projections.constructor(Master.class, roomEnrollment.user.name, roomEnrollment.user.profileImage))
+                .from(roomEnrollment)
+                .leftJoin(roomEnrollment.user, user)
+                .leftJoin(roomEnrollment.room, room)
+                .where(roomEnrollment.room.id.eq(targetRoom.getId()).and(roomEnrollment.isMaster.isTrue()))
+                .fetchOne();
+        return new RoomDetailInfo(targetRoom, master);
+    }
+
+    public List<RoomDetailInfo> findRoomDetailInfosByUser(User user) {
+        List<RoomEnrollment> roomEnrollments = queryFactory
+                .selectFrom(roomEnrollment)
+                .where(roomEnrollment.user.eq(user))
+                .fetch();
+        return roomEnrollments.stream()
+                .map(RoomEnrollment::getRoom)
+                .map(this::getMasterUser)
+                .collect(Collectors.toList());
+
     }
 }
