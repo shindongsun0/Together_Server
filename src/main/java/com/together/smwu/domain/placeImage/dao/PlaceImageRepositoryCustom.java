afdebf4fe4a19625693bf39a24dd24fa7759681b
Sindongsun <shindongsun0@naver.com>
2020-09-23 23:57:56 +0900

    add: [PlaceImage] db연동 Repository 코드 추가

--- src/main/java/com/together/smwu/domain/placeImage/dao/PlaceImageRepositoryCustom.java
+++ src/main/java/com/together/smwu/domain/placeImage/dao/PlaceImageRepositoryCustom.java
@@ -1,10 +1,5 @@
 package com.together.smwu.domain.placeImage.dao;
 
-import com.together.smwu.domain.place.domain.Place;
-import com.together.smwu.domain.placeImage.domain.PlaceImage;
-
-import java.util.List;
-
 public interface PlaceImageRepositoryCustom {
-    List<PlaceImage> findAllByPlace(Place place);
+//    List<PlaceImage> findAllByPlace(Place place);
 }
--- src/main/java/com/together/smwu/domain/placeImage/dao/PlaceImageRepositoryImpl.java
+++ src/main/java/com/together/smwu/domain/placeImage/dao/PlaceImageRepositoryImpl.java
@@ -1,24 +1,18 @@
 package com.together.smwu.domain.placeImage.dao;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.together.smwu.domain.place.domain.Place;
-import com.together.smwu.domain.placeImage.domain.PlaceImage;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
-import static com.together.smwu.domain.placeImage.domain.QPlaceImage.placeImage;
 
 @RequiredArgsConstructor
 public class PlaceImageRepositoryImpl implements PlaceImageRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<PlaceImage> findAllByPlace(Place place) {
-        return queryFactory
-                .select(placeImage)
-                .from(placeImage)
-                .where(placeImage.place.eq(place))
-                .fetch();
-    }
+//    public List<PlaceImage> findAllByPlace(Place place) {
+//        return queryFactory
+//                .select(placeImage)
+//                .from(placeImage)
+//                .where(placeImage.place.eq(place))
+//                .fetch();
+//    }
 }
