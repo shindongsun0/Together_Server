a97e5df43fca70301540c8fbd1c340e1f93158e2
Sindongsun <shindongsun0@naver.com>
2020-09-24 19:15:38 +0900

    fix: [Place] place getter메소드 추가, name과 mapX로 Place찾는 메서드 추가

--- src/main/java/com/together/smwu/domain/place/domain/Place.java
+++ src/main/java/com/together/smwu/domain/place/domain/Place.java
@@ -40,4 +40,68 @@ public class Place {
 
     @Column(name = "post_time", nullable = false)
     Timestamp postTime;
+    
+    @Builder
+    public Place(String name, Integer category, Long mapX, Long mapY,
+                 String content, String location, String mainImageUrl, Long id) {
+        this.name = name;
+        this.category = category;
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.content = content;
+        this.location = location;
+        this.mainImageUrl = mainImageUrl;
+        this.id = id;
+    }
+
+    public void update(Place place) {
+        this.name = place.name;
+        this.category = place.category;
+        this.mapX = place.mapX;
+        this.mapY = place.mapY;
+        this.content = place.content;
+        this.location = place.location;
+        this.mainImageUrl = place.mainImageUrl;
+        this.id = place.getId();
+    }
+
+    public void updateMainImageUrl(String mainImageUrl) {
+        this.mainImageUrl = mainImageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCategory() {
+        return category;
+    }
+
+    public Long getMapX() {
+        return mapX;
+    }
+
+    public Long getMapY() {
+        return mapY;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getMainImageUrl() {
+        return mainImageUrl;
+    }
+
+    public Timestamp getPostTime() {
+        return postTime;
+    }
 }
