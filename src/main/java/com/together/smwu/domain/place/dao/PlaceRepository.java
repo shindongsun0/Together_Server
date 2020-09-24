da0674cb26b758246b809fcf33dd7cab994f1935
Sindongsun <shindongsun0@naver.com>
2020-09-24 19:19:45 +0900

    add: [Place] import optional

--- src/main/java/com/together/smwu/domain/place/dao/PlaceRepository.java
+++ src/main/java/com/together/smwu/domain/place/dao/PlaceRepository.java
@@ -3,6 +3,8 @@ package com.together.smwu.domain.place.dao;
 import com.together.smwu.domain.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
     Optional<Place> findByNameAndMapX(String placeName, Long mapX);
 }
