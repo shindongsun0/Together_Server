c127d94b6b66e37a79eab8f57ab14e911db3a014
Sindongsun <shindongsun0@naver.com>
2020-10-01 00:36:00 +0900

    add: [Spring Batch] Crawling을 위한 WebDriver, selenium dependency add

---
+++ src/main/java/com/together/smwu/global/crawler/ChromeConnector.java
@@ -0,0 +1,52 @@
+package com.together.smwu.global.crawler;
+
+import lombok.Getter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@Getter
+public class ChromeConnector {
+
+    private final ChromeDriver webDriver;
+    private final WebDriverWait wait;
+
+    public ChromeConnector(ChromeDriver webDriver, WebDriverWait wait) {
+        this.webDriver = webDriver;
+        this.wait = wait;
+    }
+
+    public void get(String pageLink) {
+        webDriver.get(pageLink);
+    }
+
+    public void waitUntilByXpath(String xpath) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
+    public void waitUntilByTag(String tag) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName(tag)));
+    }
+
+    public WebElement findElementByXpath(String tag) {
+        return webDriver.findElement(By.xpath(tag));
+    }
+
+    public WebElement findElementByTag(String tag) {
+        return webDriver.findElement(By.tagName(tag));
+    }
+
+    public void waitUntilClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public WebElement findElementByClassName(String name) {
+        return webDriver.findElement(By.className(name));
+    }
+
+    public String getCurrentUrl() {
+        return webDriver.getCurrentUrl();
+    }
+}
---
+++ src/main/java/com/together/smwu/global/crawler/WebDriverConfiguration.java
@@ -0,0 +1,45 @@
+package com.together.smwu.global.crawler;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class WebDriverConfiguration {
+    @Value("${user.chromedriver}")
+    private String webDriverLocation;
+
+    public static String webDriverURL;
+
+    @PostConstruct
+    public void initURL() {
+        webDriverURL = webDriverLocation;
+    }
+
+    public static ChromeConnector createChromeConnector() {
+        System.setProperty("webdriver.chrome.driver", webDriverURL);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--whitelisted-ips");
+        options.addArguments("--no-sandbox");
+        options.addArguments(
+                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36");
+        options.addArguments("--proxy-server='direct://'");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--proxy-bypass-list=*");
+        options.addArguments("--start-maximized");
+        options.addArguments("disable-gpu");
+        options.addArguments("--lang=ko_KR");
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        options.addArguments(
+                "--Accept=text/html,application/xhtml+xml,application/xml;\\q=0.9,imgwebp,*/*;q=0.8");
+        ChromeDriver webDriver = new ChromeDriver(options);
+        WebDriverWait wait = new WebDriverWait(webDriver, 30);
+        return new ChromeConnector(webDriver, wait);
+    }
+}
